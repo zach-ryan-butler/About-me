beb5bda256d741de1f12e8c1c4cef4f2a602a686
Zach Butler <zach.standley93@gmail.com>
2019-04-23 16:45:06 -0700

    created variables inside callback function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 
 
-const submitButton = document.getElementById('submit-button')
-const activityInput = document.getElementById('activity-question')
-const colorInput = document.getElementById('color-question')
-const popularInput = document.getElementById('popular-question')
-const finalInput = document.getElementById('final-question')
-const leastInput = document.getElementById('least-question')
-const quizResult = document.getElementById('quiz-result')
+const submitButton = document.getElementById('submit-button');
+const activityInput = document.getElementById('activity-question');
+const colorInput = document.getElementById('color-question');
+const popularInput = document.getElementById('popular-question');
+const finalInput = document.getElementById('final-question');
+const leastInput = document.getElementById('least-question');
+const quizResult = document.getElementById('quiz-result');
+
+submitButton.addEventListener('click', () => {
+    const activityAnswer = activityInput.value;
+    const colorAnswer = colorInput.value;
+    const popularAnswer = popularInput.value;
+    const finalAnswer = finalInput.value;
+    const leastAnswer = leastInput.value;
+
+console.log(activityAnswer, colorAnswer, popularAnswer, finalAnswer, leastAnswer);
+});
