394359372357a6890b16fe69e2e8ca249b57a90b
Zach Butler <zach.standley93@gmail.com>
2019-04-23 16:30:24 -0700

    created variables in js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+
+const submitButton = document.getElementById('submit-button')
+const activityInput = document.getElementById('activity-question')
+const colorInput = document.getElementById('color-question')
+const popularInput = document.getElementById('popular-question')
+const finalInput = document.getElementById('final-question')
+const leastInput = document.getElementById('least-question')
+const quizResult = document.getElementById('quiz-result')
